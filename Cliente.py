8675631a18ab24c518817d3a5f923bed4e020aaa
Jéssica Guerzoni <82246276+jessguerzoni@users.noreply.github.com>
2026-03-14 18:47:47 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -14,25 +14,25 @@ nome = input("Digite seu nome: ")
 
 s.send(nome.encode())
 
-# pergunta 1
+
 p1 = s.recv(1024).decode()
 print(p1)
 r1 = input("Resposta: ")
 s.send(r1.encode())
 
-# pergunta 2
+
 p2 = s.recv(1024).decode()
 print(p2)
 r2 = input("Resposta: ")
 s.send(r2.encode())
 
-# pergunta 3
+
 p3 = s.recv(1024).decode()
 print(p3)
 r3 = input("Resposta: ")
 s.send(r3.encode())
 
-# resultado final
+
 resultado = s.recv(1024).decode()
 
 print("\nResultado:")
